da6995bb3beda6b88d66f3037a74c7647d115429
Björn <bo@kbct.de>
2017-06-04 20:08:14 +0000

    first version, http getter still hard coded

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,13 +1,75 @@
+const priColor = '#ffee00';
+const sndColor = 'black';
+
+const getters = [
+  {
+    title: 'XMR EUR',
+    url: 'https://api.kraken.com/0/public/Ticker?pair=XMREUR',
+    valueKey: 'result.XXMRZEUR.c.0',
+    valueType: 'number'
+  },
+  {
+    title: 'Nbsp Status',
+    url: 'http://status.nobreakspace.org/spaceapi.json',
+    valueKey: 'state.open',
+    valueType: 'boolean',
+    isTrue: 'Offen',
+    isFalse: 'Geschlossen',
+  },
+];
+
+function getItem(obj, key) {
+  key.split('.').forEach(function(k) {
+    obj = obj[k];
+  });
+  return obj;
+}
+
+function dataToMenuItem(getter, data) {
+  var item = getItem(data, getter.valueKey);
+  if (getter.valueType == 'number') {
+    item = (+item).toFixed(2);
+  } else if (getter.valueType == 'boolean') {
+    item = (item ? getter.isTrue : getter.isFalse) || item;
+  }
+
+  return {
+    title: getter.title,
+    subtitle: item
+  };
+}
+
 Pebble.addEventListener('ready', function() {
   require('pebblejs');
   var UI = require('pebblejs/ui');
   var ajax = require('pebblejs/lib/ajax');
 
-  var card = new UI.Card({
-    title: 'Hello World',
-    body: 'This is your first Pebble app!',
-    scrollable: true
+  var menu = new UI.Menu({
+    textColor: priColor,
+    backgroundColor: sndColor,
+    highlightTextColor: sndColor,
+    highlightBackgroundColor: priColor,
+
+    sections: [{
+        items: getters.map(function(g) {
+          return {
+            title: g.title,
+            subtitle: 'loading...'
+          };
+        })
+      }]
   });
 
-  card.show();
+  menu.status(false);
+  menu.show();
+
+  getters.forEach(function(g, i) {
+    ajax(
+      { url: g.url, type: 'json' },
+      function(data) {
+        menu.item(0, i, dataToMenuItem(g, data));
+      }
+    );
+  });
 });
+
